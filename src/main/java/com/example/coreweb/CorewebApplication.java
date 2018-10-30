e5ba20e768d81cd7e7e1e6b2c1732d1c4adc09e2
Aleksandr Kochkurkin <mailto:89636617290@mail.ru>
2018-10-30 23:03:32 +0200

    Initial commit

---
+++ src/main/java/com/example/coreweb/CorewebApplication.java
@@ -0,0 +1,12 @@
+package com.example.coreweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorewebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorewebApplication.class, args);
+    }
+}
